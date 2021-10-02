02abc7f4fb0ce26d667afc6585f72a8cf81757d9
Kevin Cerón <ceronkevin8882@gmail.com>
2021-10-01 22:38:07 -0600

    Update AccesoDatos.java

--- src/com/peliculas/datos/AccesoDatos.java
+++ src/com/peliculas/datos/AccesoDatos.java
@@ -5,10 +5,25 @@
  */
 package com.peliculas.datos;
 
+import com.peliculas.domain.Pelicula;
+import com.peliculas.excepciones.*;
+import java.util.List;
+
 /**
  *
  * @author Kevin Cerón
  */
 public interface AccesoDatos {
-    
+
+    boolean existe(String nombreArchivo) throws AccesoDatosEx;
+
+    List<Pelicula> listar(String nombre) throws LecturaDatosEx;
+
+    void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx;
+
+    String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx;
+
+    void crear(String nombreRecurso) throws AccesoDatosEx;
+
+    void borrar(String nombreRecutso) throws AccesoDatosEx;
 }
