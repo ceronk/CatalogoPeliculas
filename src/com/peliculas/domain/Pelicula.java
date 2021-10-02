d3e5eb1321cdebe5b4d514a0a282cf61ec23e274
Kevin Cerón <ceronkevin8882@gmail.com>
2021-10-01 22:30:39 -0600

    Create Pelicula.java

---
+++ src/com/peliculas/domain/Pelicula.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peliculas.domain;
+
+/**
+ *
+ * @author Kevin Cerón
+ */
+public class Pelicula {
+
+    private String nombre;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Pelicula{nombre=").append(nombre);
+        sb.append('}');
+        return sb.toString();
+    }
+}
