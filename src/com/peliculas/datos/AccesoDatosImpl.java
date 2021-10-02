8aa47f2ac48a4ecdda32456b1eea86589b59f6f7
Kevin Cerón <ceronkevin8882@gmail.com>
2021-10-02 01:46:53 -0600

    Create AccesoDatosImpl.java

---
+++ src/com/peliculas/datos/AccesoDatosImpl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peliculas.datos;
+
+import com.peliculas.domain.Pelicula;
+import com.peliculas.excepciones.AccesoDatosEx;
+import com.peliculas.excepciones.EscrituraDatosEx;
+import com.peliculas.excepciones.LecturaDatosEx;
+import java.util.List;
+
+/**
+ *
+ * @author Kevin Cerón
+ */
+public class AccesoDatosImpl implements IAccesoDatos{
+
+    @Override
+    public boolean existe(String nombreArchivo) throws AccesoDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Pelicula> listar(String nombre) throws LecturaDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void crear(String nombreRecurso) throws AccesoDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void borrar(String nombreRecutso) throws AccesoDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
