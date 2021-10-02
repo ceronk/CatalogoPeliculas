658850c1bcbf29c50f7755f4ba3b7a16a7eeb1f4
Kevin Cerón <ceronkevin8882@gmail.com>
2021-10-02 01:47:49 -0600

    Update AccesoDatosImpl.java

--- src/com/peliculas/datos/AccesoDatosImpl.java
+++ src/com/peliculas/datos/AccesoDatosImpl.java
@@ -47,4 +47,5 @@ public class AccesoDatosImpl implements IAccesoDatos{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    
 }
