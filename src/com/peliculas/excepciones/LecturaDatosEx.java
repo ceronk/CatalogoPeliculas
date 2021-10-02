25c20d0a3604d575ced79fab90ea2ee206f35d2d
Kevin Cerón <ceronkevin8882@gmail.com>
2021-10-01 22:30:45 -0600

    Create LecturaDatosEx.java

---
+++ src/com/peliculas/excepciones/LecturaDatosEx.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peliculas.excepciones;
+
+/**
+ *
+ * @author Kevin Cerón
+ */
+public class LecturaDatosEx extends AccesoDatosEx{
+    
+    public LecturaDatosEx(String mensaje) {
+        super(mensaje);
+    }
+    
+}
