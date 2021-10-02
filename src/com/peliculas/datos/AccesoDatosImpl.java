32ae439c6077d089072343984a471f63e3843843
Kevin Cerón <ceronkevin8882@gmail.com>
2021-10-02 04:01:20 -0600

    Update AccesoDatosImpl.java

--- src/com/peliculas/datos/AccesoDatosImpl.java
+++ src/com/peliculas/datos/AccesoDatosImpl.java
@@ -14,10 +14,10 @@ import java.util.*;
  *
  * @author Kevin Cerón
  */
-public class AccesoDatosImpl implements IAccesoDatos{
+public class AccesoDatosImpl implements IAccesoDatos {
 
     @Override
-    public boolean existe(String nombreRecurso) throws AccesoDatosEx {
+    public boolean existe(String nombreRecurso) {
         File archivo = new File(nombreRecurso);
         return archivo.exists();
     }
@@ -25,33 +25,92 @@ public class AccesoDatosImpl implements IAccesoDatos{
     @Override
     public List<Pelicula> listar(String nombre) throws LecturaDatosEx {
         File archivo = new File(nombre);
-        List<Pelicula> pelicula = new ArrayList<>(); 
+        List<Pelicula> peliculas = new ArrayList<>();
         try {
             BufferedReader entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            while (linea != null) {
+                Pelicula pelicula = new Pelicula(linea);
+                peliculas.add(pelicula);
+                linea = entrada.readLine();
+            }
+            entrada.close();
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
             throw new LecturaDatosEx("Excepción al listar peliculas: " + ex.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepción al listar peliculas: " + ex.getMessage());
         }
-        return pelicula;
+        return peliculas;
     }
 
     @Override
     public void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx {
-        
+        PrintWriter salida = null;
+        try {
+            File archivo = new File(nombreRecurso);
+            salida = new PrintWriter(new FileWriter(archivo, anexar));
+            salida.println(pelicula.toString());
+            salida.close();
+            System.out.println("Se ha escrito información al archivo: " + pelicula);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new EscrituraDatosEx("Excepción al listar peliculas: " + ex.getMessage());
+        }
     }
 
     @Override
     public String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx {
-        
+
+        File archivo = new File(nombreRecurso);
+        String resultado = null;
+
+        try {
+            BufferedReader entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            int indice = 1;
+            while (linea != null) {
+                if (buscar != null && buscar.equalsIgnoreCase(linea)) {
+                    resultado = "Pelicula " + linea + " encontrada en el indice: " + indice;
+                    break;
+                }
+                linea = entrada.readLine();
+                indice++;
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepción al buscar peliculas: " + ex.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepción al buscar peliculas: " + ex.getMessage());
+        }
+        return resultado;
     }
 
     @Override
     public void crear(String nombreRecurso) throws AccesoDatosEx {
-        
+        File archivo = new File(nombreRecurso);
+        try {
+            PrintWriter salida = new PrintWriter(new FileWriter(archivo));
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new AccesoDatosEx("Excepción al crear archivo: " + ex.getMessage());
+        }
     }
 
     @Override
-    public void borrar(String nombreRecutso) throws AccesoDatosEx {
-        
+    public void borrar(String nombreRecurso) {
+        File archivo = new File(nombreRecurso);
+        if (archivo.exists()) {
+            archivo.delete();
+            System.out.println("Se ha borrado el archivo");
+        }
+
     }
 }
